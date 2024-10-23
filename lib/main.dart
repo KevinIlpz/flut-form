4c84aa5b22037856e5a689a5920123866a126fd8
KevinIlpz <kevin.02.april@gmail.com>
2024-10-23 10:48:22 +0700

    upload

---
+++ lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'page2.dart';
+import 'page3.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Page3(),
+));
+
+class home extends StatelessWidget {
+  final String username;
+  final String role;
+  final String school;
+  final String description;
+
+  const home({
+    Key? key,
+    required this.username,
+    required this.role,
+    required this.school,
+    required this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.grey, Colors.red],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  elevation: 10,
+                  color: Color.fromARGB(9, 26, 11, 22).withOpacity(0.9),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 80.0,
+                          backgroundImage: AssetImage("assets/img.png"),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          username,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          role,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromRGBO(225, 247, 245, 1),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          school,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromRGBO(225, 247, 245, 1),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          description,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromRGBO(225, 247, 245, 0.8),
+                          ),
+                        ),
+                        SizedBox(height: 30),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => page2()),
+                            );
+                          },
+                          child: Text(
+                            'See More',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.teal,
+                            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            elevation: 5,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/page3.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+class Page3 extends StatefulWidget {
+  @override
+  _Page3State createState() => _Page3State();
+}
+
+class _Page3State extends State<Page3> with SingleTickerProviderStateMixin {
+  bool _isFormVisible = false;
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _roleController = TextEditingController();
+  final TextEditingController _schoolController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 700),
+    );
+    _fadeAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    );
+
+    Future.delayed(Duration(milliseconds: 500), () {
+      setState(() {
+        _isFormVisible = true;
+      });
+      _animationController.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _usernameController.dispose();
+    _roleController.dispose();
+    _schoolController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.red.shade400, Colors.black],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+          Center(
+            child: FadeTransition(
+              opacity: _fadeAnimation,
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Card(
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(17),
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Padding(
+                    padding: EdgeInsets.all(25),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Welcome!',
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black, // Changed text color to black
+                            letterSpacing: 1.9,
+                          ),
+                        ),
+                        SizedBox(height: 40),
+                        _buildTextInput(
+                          hint: 'Username',
+                          icon: Icons.person_outline,
+                          controller: _usernameController,
+                        ),
+                        SizedBox(height: 20),
+                        _buildTextInput(
+                          hint: 'Role',
+                          icon: Icons.work_outline,
+                          controller: _roleController,
+                        ),
+                        SizedBox(height: 20),
+                        _buildTextInput(
+                          hint: 'School',
+                          icon: Icons.school_outlined,
+                          controller: _schoolController,
+                        ),
+                        SizedBox(height: 20),
+                        _buildTextInput(
+                          hint: 'Description',
+                          icon: Icons.description_outlined,
+                          controller: _descriptionController,
+                        ),
+                        SizedBox(height: 30),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => home(
+                                  username: _usernameController.text,
+                                  role: _roleController.text,
+                                  school: _schoolController.text,
+                                  description: _descriptionController.text,
+                                ),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: Size(double.infinity, 50),
+                            backgroundColor: Colors.grey, // Changed button color to grey
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            shadowColor: Colors.grey[700],
+                            elevation: 10,
+                          ),
+                          child: Text(
+                            'SUBMIT',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black, // Changed text color to black
+                              letterSpacing: 1.2,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextInput({
+    required String hint,
+    required IconData icon,
+    required TextEditingController controller,
+    bool obscureText = false,
+  }) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        prefixIcon: Icon(icon, color: Colors.black),  // Changed icon color to black
+        hintText: hint,
+        hintStyle: TextStyle(color: Colors.black),  // Changed hint text color to black
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.8),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      style: TextStyle(color: Colors.black),  // Changed input text color to black
+    );
+  }
+}
